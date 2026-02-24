e35e3fa032930f9d5643572b4a3509b38dda47d1
Renzo Johnson <4695400+renzojohnson@users.noreply.github.com>
2026-02-24 16:27:49 -0500

    Add file header docblocks with author info to all PHP files

--- src/Database/TableResolver.php
+++ src/Database/TableResolver.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * WC HPOS Helper
+ *
+ * @package   RenzoJohnson\WcHposHelper
+ * @author    Renzo Johnson <hello@renzojohnson.com>
+ * @copyright 2026 Renzo Johnson
+ * @license   MIT
+ * @link      https://renzojohnson.com
+ */
+
 declare(strict_types=1);
 
 namespace RenzoJohnson\WcHposHelper\Database;
